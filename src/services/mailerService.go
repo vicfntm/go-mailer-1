105461386ef1db72eea594b6b4e4f5542891112e
victorderevyanko <4944995@gmail.com>
2022-08-26 12:41:37 +0300

    rearrangement

--- src/services/mailerService.go
+++ src/services/mailerService.go
@@ -16,6 +16,9 @@ type MailerService struct {
 	adminEmail string
 }
 
+var SUBJECT = "client feedback form \n"
+var MIME_TYPE = "MIME-version: 1.0;\nContent-Type: text/plain; charset=\"UTF-8\";\n\n"
+
 func (mailer *MailerService) Push(pack models.FeedbackForm) (bool, error) {
 
 	target := []string{mailer.adminEmail}
@@ -34,10 +37,7 @@ func (mailer *MailerService) Push(pack models.FeedbackForm) (bool, error) {
 func (mailer *MailerService) SendEmailSMTP(to []string, body string) (bool, error) {
 
 	emailAuth := smtp.PlainAuth("", mailer.emailLogin, mailer.password, mailer.host)
-
-	mime := "MIME-version: 1.0;\nContent-Type: text/plain; charset=\"UTF-8\";\n\n"
-	subject := "Subject: " + "Test Email" + "!\n"
-	msg := []byte(subject + mime + "\n" + body)
+	msg := []byte(SUBJECT + MIME_TYPE + "\n" + body)
 	addr := fmt.Sprintf("%s:%s", mailer.host, mailer.port)
 
 	if err := smtp.SendMail(addr, emailAuth, mailer.emailLogin, to, msg); err != nil {
